26064a8ec6bbffe74e13ed076bc64836673c45b9
RoniBlade <78499428+RoniBlade@users.noreply.github.com>
2022-08-18 09:16:17 +0300

    New version program
    V.2

--- Project1/Project1/Films.h
+++ Project1/Project1/Films.h
@@ -14,85 +14,136 @@ public:
 	ofstream fout;
 	ifstream fin;
 
-	struct kino
+	//struct kino
+	//{
+	//	int sell;
+	//	string nameis;
+	//	string date;
+	//};
+
+	
+	struct teacher
 	{
-		int sell;
-		string nameis;
+		int numberHours;
+		string teacherName;
+		string department;
+		string title;
+		string degree;
+		string discipline;
 		string date;
 	};
 
-	int id;
-	string name;
-	string desc;
-	string shanr;
-	int time;
-	float raiting;
-	kino kinos[100];
+	struct numberHours
+	{
+		int numberLectureHours;
+		int numberPracticalHours;
+		int numberHoursLaboratoryWork;
+		int totalHours;
+	};
+
+
+	teacher teachers;
+	numberHours listOfNumberHours;
 	int c;
 	string help;
 
+	//counter
+	int count = 0;
+
 	//new data
-	int directionCode;
+	int directionCodeInt;
+	string directionCode;
 	string nameDiscipline;
 	string discipline;
 	int course;
 	int term;
-	string signReporting;
-	string teacher;
-	int numberHours;
-
-
-
-
-
+	int test;
+	int exam;
+	int courseWork;
+	int courseProject;
+	//teacher data
+	string tmp;
+	string tmp_str;
+	string tmp3;
 
 	void intodata()
 	{
 		cin.ignore();
 		cout << "Код направления" << endl;
-		getline(cin, name);
-		cout << "Название дисциплины:" << endl;
-		getline(cin, desc);
-		cout << "Дисциплина: " << endl;
-		getline(cin, shanr);
-		cout << "Курс:" << endl;
-		cin >> time;
-		cout << "Семестр:" << endl;
-		cin >> raiting;
-		cout << "признак отчетности :" << endl;
-		cin >> c;
-		for (int i = 0; i < c; i++)
+		cin >> directionCode;
+		tmp = getDirectionCode();
+		tmp3 = getDirectionCode();
+		int a = getDirectionCode().length();
+		//Получаем все символы кроме точек и записываем их в новую строку
+		for (int j = 0; j < a; j++)
 		{
-			cout << "Преподаватель:" << endl;
-			cin.ignore();
-			getline(cin, help);
-			kinos[i].nameis = help;
-			cout << "Количество часов :" << endl;
-			cin >> kinos[i].sell;
-			//cout << ":" << endl;
+			if (tmp[j] != '.')
+				tmp_str += (tmp[j]);
 		}
+		//Преобразуем в число
+		directionCodeInt = stoi(tmp_str);
+
+		cout << "Название дисциплины:" << endl;
+		cin >> nameDiscipline;
+		cout << "Дисциплина:" << endl;
+		cin >> discipline;
+		cout << "Курс:" << endl;
+		cin >> course;
+		cout << "Семестр:" << endl;
+		cin >> term;
+		cout << "Признак отчетности :\n1)Зачет:" << endl;
+		cin >> test;
+		cout << "2)Экзамен:" << endl;
+		cin >> exam;
+		cout << "2)Курсовая работа:" << endl;
+		cin >> courseWork;
+		cout << "4)Курсовой проект:" << endl;
+		cin >> courseProject;
+		cout << "Преподаватель\n1) ФИО:" << endl;
+		cin >> teachers.teacherName;
+		cout << "2) Кафедра:" << endl;
+		cin >> teachers.department;
+		cout << "3) Звание:" << endl;
+		cin >> teachers.title;
+		cout << "4) Степень:" << endl;
+		cin >> teachers.degree;
+		cout << "5) Дисциплина:" << endl;
+		cin >> teachers.discipline;
+		cout << "Количество часов:\n1) Количество лекционных часов: " << endl;
+		cin >> listOfNumberHours.numberLectureHours;
+		cout << "2) Количество практических часов:" << endl;
+		cin >> listOfNumberHours.numberPracticalHours;
+		cout << "3) Количество часов на лабораторные работы: " << endl;
+		cin >> listOfNumberHours.numberHoursLaboratoryWork;
+		cout << "4) Всего часов:" << endl;
+		cin >> listOfNumberHours.totalHours;
 		system("cls");
-		cout << "Новый направление успешно добавлен!";
+		cout << "Новое направление успешно добавлено!";
+
 	}
 
 	void outdata()
 	{
-		system("cls");
-		cout << "\n\nНазвание маршрута: ";
-		cout << name << endl;
-		cout << "Описание маршрута: ";
-		cout << desc << endl;
-		cout << "Пересандка:" << endl;
-		cout << shanr << endl;
-		cout << "Номер рейса: ";
-		cout << time << " " << endl;
-		cout << "Количество мест: ";
-		cout << raiting << endl;
-		cout << "Количество классов: " << endl;
-		for (int i = 0; i < c; i++)
-		{
-			cout << i + 1 << ") " << kinos[i].nameis << " Цена: " << kinos[i].sell << endl;
-		}
+		cin.ignore();
+		cout << "Код направления: " << directionCode << endl;
+		cout << "Название дисциплины: " << nameDiscipline << endl;
+		cout << "Дисциплина: " << discipline << endl;
+		cout << "Курс: " << course << endl;
+		cout << "Семестр: " << term << endl;
+		cout << "Признак отчетности :\n1)Зачет:" << test << endl;
+		cout << "2)Экзамен: " << exam << endl;
+		cout << "2)Курсовая работа: " << courseWork << endl;
+		cout << "4)Курсовой проект: " << courseProject << endl;
+		cout << "Преподаватель\n1) ФИО: " << teachers.teacherName << endl;
+		cout << "2) Кафедра: " << teachers.department << endl;
+		cout << "3) Звание: " << teachers.title << endl;
+		cout << "4) Степень: " << teachers.degree << endl;
+		cout << "5) Дисциплина: " << teachers.discipline << endl;
+		cout << "Количество часов:\n1) Количество лекционных часов: " << listOfNumberHours.numberLectureHours << endl;
+		cout << "2) Количество практических часов: " << listOfNumberHours.numberPracticalHours << endl;
+		cout << "3) Количество часов на лабораторные работы: " << listOfNumberHours.numberHoursLaboratoryWork << endl;
+		cout << "4) Всего часов: " << listOfNumberHours.totalHours << endl;
+		//system("cls");
 		cout << "\nНажмите любую для продолжения...";
 	}
 	void clrtxt()
@@ -106,17 +157,24 @@ public:
 		fout.open("kino.txt", ios_base::app);
 		fout << max << endl;
 		fout << "#" << id1 << endl;
-		fout << name << endl;
-		fout << desc << endl;
-		fout << shanr << endl;
-		fout << time << endl;
-		fout << raiting << endl;
-		fout << c << endl;
-		for (int i = 0; i < c; i++)
-		{
-			fout << kinos[i].nameis << endl;
-			fout << kinos[i].sell << endl;
-		}
+		fout << directionCode << endl;
+		fout << nameDiscipline << endl;
+		fout << discipline << endl;
+		fout << course << endl;
+		fout << term << endl;
+		fout << test << endl;
+		fout << exam << endl;
+		fout << courseWork << endl;
+		fout << courseProject << endl;
+		fout << teachers.teacherName << endl;
+		fout << teachers.department << endl;
+		fout << teachers.title << endl;
+		fout << teachers.degree << endl;
+		fout << teachers.discipline << endl;
+		fout << listOfNumberHours.numberLectureHours << endl;
+		fout << listOfNumberHours.numberPracticalHours << endl;
+		fout << listOfNumberHours.numberHoursLaboratoryWork << endl;
+		fout << listOfNumberHours.totalHours << endl;
 		fout.close();
 		system("cls");
 		cout << "Вы успешно сохранили!";
@@ -141,65 +199,60 @@ public:
 		{
 			if (line[0] == '#' && line[1] == (char)(id1 + 48))
 			{
-				getline(fin, name);
-				getline(fin, desc);
-				getline(fin, shanr);
-				fin >> time;
-				fin >> raiting;
-				fin >> c;
-				for (int i = 0; i < c; i++)
-				{
-					fin.ignore();
-					getline(fin, helper);
-					kinos[i].nameis = helper;
-					fin >> ihelp;
-					kinos[i].sell = ihelp;
-				}
+				fin >> directionCode;
+				fin >> nameDiscipline;
+				fin >> discipline;
+				fin >> course;
+				fin >> term;
+				fin >> test;
+				fin >> exam;
+				fin >> courseWork;
+				fin >> courseProject;
+				fin >> teachers.teacherName;
+				fin >> teachers.department;
+				fin >> teachers.title;
+				fin >> teachers.degree;
+				fin >> teachers.discipline;
+				fin >> listOfNumberHours.numberLectureHours;
+				fin >> listOfNumberHours.numberPracticalHours;
+				fin >> listOfNumberHours.numberHoursLaboratoryWork;
+				fin >> listOfNumberHours.totalHours;
 			}
 		}
 		fin.close();
 	}
 
-	float getraiting()
+
+
+
+
+	string getDirectionCode()
 	{
-		return raiting;
+		return directionCode;
+	}
+	int getDirectionCodeInt()
+	{
+		return directionCodeInt;
 	}
 
-	string getname()
+	string getNameDiscipline()
 	{
-		return name;
+		return nameDiscipline;
 	}
 
-	string getkinosnameis(int f)
+	int getTest() 
 	{
-		for (int i = 0; i < c; i++)
-		{
-			if (kinos[i].sell == f)
-				return kinos[i].nameis;
-		}
+		return test;
 	}
 
-
-
-
-	int gettime()
+	int getListOfNumberHoursTotalHours()
 	{
-		return time;
+		return listOfNumberHours.totalHours;
 	}
 
-	int getc()
-	{
-		return c;
-	}
+	string getTeacherName() {
 
-	int getsell(int i)
-	{
-		return kinos[i].sell;
-	}
-
-	string getkino(int i)
-	{
-		return kinos[i].nameis;
+		return teachers.teacherName;
 	}
 
 };
--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -14,157 +14,268 @@ struct arrs
 	float fl;
 	bool bb = false;
 };
-
-Films fi[100];
+Films fi[10];
 arrs myarr[100];
 arrs timearr[100];
 arrs chararr[100];
 arrs chararrr[100];
 arrs raiting[100];
 
-void sortf(int s)
+//сортировка пузырьком для кода направления
+void sortByCode(int max)
 {
-	for (int i = 0; i < s - 1; i++)
-	{
-		int small = i;
 
-		for (int j = i + 1; j < s; j++)
+
+	string list[10] = {};
+	for (int i = 0; i < max; i++)
+	{
+		
+		list[i] = fi[i].getDirectionCode();
+		
+	}
+
+	int listLength = max;
+
+	while (listLength--)
+	{
+		bool swapped = false;
+
+		for (int i = 0; i < listLength; i++)
 		{
-			if (raiting[j].fl < raiting[small].fl)
-				small = j;
-		}
-		swap(raiting[i].fl, raiting[small].fl);
-		swap(raiting[i].i, raiting[small].i);
-	}
-	system("cls");
-	cout << "   Название рейса | Номер самолета | Кол-во мест" << endl << endl;
-	for (int i = 0; i < s; i++)
-	{
-
-		cout << raiting[i].i + 1 << ") ";
-		int t = raiting[i].i;
-		cout << fi[t].getname() << " | ";
-		cout << fi[t].gettime() << " ";
-		cout << " | " << fi[t].getraiting() << endl;
-	}
-	int answ;
-	cout << "\n Введите найденый рейс: ";
-	cin >> answ;
-	fi[answ - 1].outdata();
-}
-
-void sortcc(int s)
-{
-	for (int i = 0; i < s - 1; i++)
-		for (int j = i; j >= 0; j--)
-			if (chararrr[j].name > chararrr[j + 1].name)
+			if (list[i] > list[i + 1])
 			{
-				swap(chararrr[j].name, chararrr[j + 1].name);
-				swap(chararrr[j].i, chararrr[j + 1].i);
+				swap(list[i], list[i + 1]);
+				swapped = true;
 			}
+		}
 
-	system("cls");
-	cout << "   Класс | Название рейса | Номер самолета | Кол-во мест" << endl << endl;
-	for (int i = 0; i < s; i++)
+		if (swapped == false)
+			break;
+	}
+
+	cout << "\nКод направления\n";
+	for (int i = 0; i < max; i++)
+	{
+		cout << i + 1 << ") " << list[i] << endl;
+	}
+	int answ;
+	cout << "\n Выберите: ";
+	cin >> answ;
+	for (int i = 0; i < max; i++)
 	{
 
-		cout << chararrr[i].i + 1 << ") ";
-		int t = chararrr[i].i;
-		cout << chararrr[i].name << " | ";
-		cout << fi[t].getname();
-		cout << " | " << fi[t].gettime() << " ";
-		cout << " | " << fi[t].getraiting() << endl;
-	}
-}
+		if (list[i] == fi[answ - 1].getDirectionCode())
+		{
+			fi[i].outdata();
+		}
 
-void sortc(int s)
+	}
+
+}
+	//по дисциплинам		
+void sortByNameDiscipline(int max)
 {
-	for (int i = 0; i < s - 1; i++)
-		for (int j = i; j >= 0; j--)
-			if (chararr[j].name > chararr[j + 1].name)
+
+	string list[10] = {};
+	for (int i = 0; i < max; i++)
+	{
+
+		list[i] = fi[i].getNameDiscipline();
+
+	}
+
+	int listLength = max;
+	
+	while (listLength--)
+	{
+		bool swapped = false;
+
+		for (int i = 0; i < listLength; i++)
+		{
+			if (list[i] > list[i + 1])
 			{
-				swap(chararr[j].name, chararr[j + 1].name);
-				swap(chararr[j].i, chararr[j + 1].i);
+				swap(list[i], list[i + 1]);
+				swapped = true;
 			}
+		}
 
-	system("cls");
-	cout << "   Название рейса | Номер самолета | кол-во мест" << endl << endl;
-	for (int i = 0; i < s; i++)
+		if (swapped == false)
+			break;
+	}
+	cout << "\nДисциплина\n";
+	for (int i = 0; i < max; i++)
 	{
-
-		cout << chararr[i].i + 1 << ") ";
-		int t = chararr[i].i;
-		cout << chararr[i].name << " | ";
-		cout << fi[t].gettime() << " ";
-		cout << " | " << fi[t].getraiting() << endl;
+		cout << i + 1 << ") " << list[i] << endl;
 	}
 	int answ;
-	cout << "\n Введите найденый маршрут: ";
+	cout << "\n Выберите: ";
 	cin >> answ;
-	fi[answ - 1].outdata();
-}
-
-void sortl(int s)
-{
-	for (int i = 0; i < s - 1; i++)
+	for (int i = 0; i < max; i++)
 	{
-		int small = i;
 
-		for (int j = i + 1; j < s; j++)
+		if (list[i] == fi[answ - 1].getNameDiscipline())
 		{
-			if (timearr[j].arr < timearr[small].arr)
-				small = j;
+			fi[i].outdata();
 		}
-		swap(timearr[i].arr, timearr[small].arr);
-		swap(timearr[i].i, timearr[small].i);
+
 	}
-	system("cls");
-	cout << "   Название рейса | Номер самолета | Кол-во мест" << endl << endl;
-	for (int i = 0; i < s; i++)
+
+
+}
+//Сортировка по зачетам
+void sortByTest(int max)
+{
+
+
+	int list[10] = {};
+	for (int i = 0; i < max; i++)
 	{
 
-		cout << timearr[i].i + 1 << ") ";
-		int t = timearr[i].i;
-		cout << fi[t].getname() << " | ";
-		cout << timearr[i].arr << " ";
-		cout << " | " << fi[t].getraiting() << endl;
+		list[i] = fi[i].getTest();
+
+	}
+
+	int listLength = max;
+
+	while (listLength--)
+	{
+		bool swapped = false;
+
+		for (int i = 0; i < listLength; i++)
+		{
+			if (list[i] > list[i + 1])
+			{
+				swap(list[i], list[i + 1]);
+				swapped = true;
+			}
+		}
+
+		if (swapped == false)
+			break;
+	}
+	cout << "\nКоличество зачетов\n";
+	for (int i = 0; i < max; i++)
+	{
+		cout << i + 1 << ") " << list[i] << endl;
 	}
 	int answ;
-	cout << "\n Введите найденый товар: ";
+	cout << "\n Выберите: ";
 	cin >> answ;
-	fi[answ - 1].outdata();
+	for (int i = 0; i < max; i++)
+	{
+
+		if (list[i] == fi[answ - 1].getTest())
+		{
+			fi[i].outdata();
+		}
+
+	}
+
 }
 
-void sorts(int s)
+//по преподавателям	
+void sortByNameTeacher(int max)
 {
 
-	for (int i = 0; i < s - 1; i++)
+	string list[10] = {};
+	for (int i = 0; i < max; i++)
 	{
-		int small = i;
 
-		for (int j = i + 1; j < s; j++)
+		list[i] = fi[i].getTeacherName();
+
+	}
+
+	int listLength = max;
+
+	while (listLength--)
+	{
+		bool swapped = false;
+
+		for (int i = 0; i < listLength; i++)
 		{
-			if (myarr[j].arr < myarr[small].arr)
-				small = j;
+			if (list[i] > list[i + 1])
+			{
+				swap(list[i], list[i + 1]);
+				swapped = true;
+			}
 		}
-		swap(myarr[i].arr, myarr[small].arr);
-		swap(myarr[i].i, myarr[small].i);
+
+		if (swapped == false)
+			break;
 	}
-	system("cls");
-	cout << "   Название рейса | Классы | Цена | Кол-во мест" << endl << endl;
-	for (int i = 0; i < s; i++)
+
+	cout << "\nФИО Преподавателя\n";
+	for (int i = 0; i < max; i++)
+	{
+		cout << i + 1 << ") " << list[i] << endl;
+	}
+	int answ;
+	cout << "\n Выберите: ";
+	cin >> answ;
+	for (int i = 0; i < max; i++)
 	{
 
-		cout << myarr[i].i + 1 << ") ";
-		int t = myarr[i].i;
-		cout << fi[t].getname() << " | ";
-		cout << fi[t].getkinosnameis(myarr[i].arr);
-		cout << " | " << myarr[i].arr;
-		cout << " | " << fi[t].getraiting() << endl;
+			if(list[i] == fi[answ - 1].getTeacherName())
+			{
+				fi[i].outdata();
+			}
+	
+	}
+	
 
+}
+
+//Сортировка по количеству часов
+void sortByCountHours(int max)
+{
+
+
+	int list[10] = {};
+	for (int i = 0; i < max; i++)
+	{
+
+		list[i] = fi[i].getListOfNumberHoursTotalHours();
 
 	}
+
+	int listLength = max;
+
+	while (listLength--)
+	{
+		bool swapped = false;
+
+		for (int i = 0; i < listLength; i++)
+		{
+			if (list[i] > list[i + 1])
+			{
+				swap(list[i], list[i + 1]);
+				swapped = true;
+			}
+		}
+
+		if (swapped == false)
+			break;
+	}
+	cout << "\nКоличество часов\n";
+	for (int i = 0; i < max; i++)
+	{
+		cout << i + 1 << ") " << list[i] << endl;
+	}
+	int answ;
+	cout << "\n Выберите: ";
+	cin >> answ;
+	for (int i = 0; i < max; i++)
+	{
+
+		if (list[i] == fi[answ - 1].getListOfNumberHoursTotalHours())
+		{
+			fi[i].outdata();
+		}
+
+	}
+
 }
+
 
 int main()
 {
@@ -190,7 +301,7 @@ int main()
 		if (answ == 1)
 		{
 			for (int i = 1; i < max + 1; i++)
-				cout << i << ") " << fi[i - 1].getname() << endl;
+				cout << i << ") " << fi[i - 1].getDirectionCode() << endl;
 			if (max == 0)
 				cout << "Нет информации" << endl;
 			else
@@ -236,139 +347,39 @@ int main()
 		if (answ == 5)
 		{
 
-			cout << "1) Сортировка по классам" << endl;
-			cout << "2) Сортировка по рейсам" << endl;
-			cout << "3) Сортировка по номеру самолета" << endl;
-			cout << "4) Сортировка по ценам" << endl;
-			cout << "5) Сортировка по количество мест" << endl;
-			cout << "6) Поиск названия" << endl;
+			cout << "1) Сортировка по кодам направления" << endl;
+			cout << "2) Сортировка по названиям дисциплины" << endl;
+			cout << "3) Сортировка по признакам отчетности" << endl;
+			cout << "4) Сортировка по преподавателям" << endl;
+			cout << "5) Сортировка по количеству часов" << endl;
+
 
 			cin >> answ;
 
 			if (answ == 1)
 			{
-				int s = 0;
-				for (int i = 0; i < max; i++)
-				{
-					int c;
+				sortByCode(max);
 
-					c = fi[i].getc();
-					for (int j = 0; j < c; j++)
-					{
-						chararrr[s].name = fi[i].getkino(j);
-						chararrr[s].i = i;
-						s++;
-					}
-
-				}
-				sortcc(s);
 			}
 
 			if (answ == 2)
 			{
-				for (int i = 0; i < max; i++)
-				{
-					chararr[i].name = fi[i].getname();
-					chararr[i].i = i;
-				}
-				sortc(max);
+				sortByNameDiscipline(max);
 			}
 
 			if (answ == 3)
 			{
-				for (int i = 0; i < max; i++)
-				{
-					timearr[i].arr = fi[i].gettime();
-					timearr[i].i = i;
-				}
-				sortl(max);
+				sortByTest(max);
 			}
 
 			if (answ == 4)
 			{
-				int c;
-				int s = 0;
-
-				for (int i = 0; i < max; i++)
-				{
-					c = fi[i].getc();
-					for (int j = 0; j < c; j++)
-					{
-						myarr[s].arr = fi[i].getsell(j);
-						myarr[s].i = i;
-						s++;
-					}
-
-				}
-				sorts(s);
+				sortByNameTeacher(max);
 			}
 			if (answ == 5)
 			{
-				for (int i = 0; i < max; i++)
-				{
-					raiting[i].fl = fi[i].getraiting();
-					raiting[i].i = i;
-				}
-				sortf(max);
+				sortByCountHours(max);
 			}
-			if (answ == 6)
-			{
-				string poisk;
-				system("cls");
-				cout << "Ввод в поисковик: ";
-				cin.ignore();
-				getline(cin, poisk);
-
-				setlocale(LC_ALL, "Russian");
-
-				for (int i = 0; i < max; i++)
-				{
-					chararr[i].name = fi[i].getname();
-					chararr[i].i = i;
-				}
-
-				for (int i = 0; i < max; i++)
-				{
-					for (int j = 0; j < poisk.length(); j++)
-					{
-						if (chararr[i].name[j] != poisk[j])
-							break;
-						else
-							if (j == poisk.length() - 1)
-								chararr[i].bb = true;
-
-					}
-				}
-				bool su = false;
-				system("cls");
-				cout << "   Название техники | Телефон | Скидка" << endl << endl;
-				for (int i = 0; i < max; i++)
-				{
-					if (chararr[i].bb)
-					{
-						cout << chararr[i].i + 1 << ") ";
-						int t = chararr[i].i;
-						cout << fi[t].getname() << " | ";
-						cout << fi[t].gettime() << " ";
-						cout << " | " << fi[t].getraiting() << endl;
-						su = true;
-						chararr[i].bb = false;
-					}
-				}
-				if (su)
-				{
-					cout << "\n Введите найденый рейс: ";
-					cin >> answ;
-					fi[answ - 1].outdata();
-					su = false;
-				}
-				else
-				{
-					system("cls");
-					cout << "Увы! Ничего не найдено";
-				}
-			}
-
 
 		}
 		if (answ == 0)
